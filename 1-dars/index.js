766a8da33bf2f433087897773889137c55c54f7f
BahromWeb <bahromhasanov0202@gmail.com>
2025-05-07 09:43:47 +0500

    all change

---
+++ 1-dars/index.js
@@ -0,0 +1,58 @@
+// '', "", ``
+
+// let name = "Webbrain Academy";
+
+// console.log("Welcome " + name);
+// console.log("Welcome \nto 'Name'");
+
+// console.log(true);
+// console.log(false);
+// ==============
+
+// null
+// let a;
+// let b = null;
+
+// console.log(null === undefined);
+// console.log(null == undefined);
+
+// ==============
+// let a;
+// let b = null;
+// console.log(null + 1);
+// console.log(undefined + 1);
+// console.log(a + 1);
+// console.log(b + 1);
+
+// ==============
+// symbols
+// let a = Symbol("text");
+// let b = Symbol("text");
+
+// console.log(b === a);
+// console.log(b == a);
+
+// ==============
+// Object - array, obj, function
+
+// let ar = ["Muhammad", 12, "Iqboljon"]; // index
+// let obj = { name: "Bahrom", status: "IT Company" }; // key
+// let func = () => {};
+// function test() {
+//   console.log(ar[0]);
+//   console.log(obj.name);
+// }
+
+// test();
+
+// console.log(typeof func);
+
+
+// amaliy mashqlar
+
+
+// Kubning yon tomoni a berilgan. Uning hajmini V=a*a*a va to’la sirti 
+// S=6*a*a aniqlansin.
+
+// masala 1
+
