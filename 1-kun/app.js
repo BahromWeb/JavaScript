ec5f4089a904a94a29f5a53f8fa6c69a8a6c9de0
BahromWeb <bahromhasanov0202@gmail.com>
2025-06-23 22:19:56 +0500

    new example

---
+++ 1-kun/app.js
@@ -0,0 +1,39 @@
+
+// 1. Son juftmi yoki toqmi?
+//  Sharti:
+// Son beriladi. Agar son juft bo‘lsa "Juft", agar toq bo‘lsa "toq" so‘zini qaytaring.
+
+// const checkNumber=(num)=>{
+//     return num%2===0?"Juft":"toq"
+// }
+
+// console.log(checkNumber(3))
+// console.log(checkNumber(6))
+// console.log(checkNumber(8))
+
+//  2. Ikkita sonning yig'indisi
+//  Sharti:
+// Ikkita son berilgan. Ularning yig'indisini qaytaring.
+
+// const sum=(a, b) => a+b
+
+// console.log(sum(2, 4))
+// console.log(sum(12, 5))
+
+// 3. Massiv uzunligini topish
+//  Sharti:
+// Berilgan massivning uzunligini (nechta element borligini) qaytaring.
+
+// let arr = [2, 45, 56, 6, 45, 23, 67, 68];
+
+// const lengthOfArray = (arr) => arr.length;
+
+// console.log(lengthOfArray(arr));
+
+// 4. Massivga element qo‘shish
+// Sharti:
+// Massiv va bitta element beriladi. Uni massiv oxiriga qo‘shing.
+
+const addToArray=()=>{
+    
+}
