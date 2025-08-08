e317b5b334fe44d3f7e9b542f2be293b6675a9c3
BahromWeb <bahromhasanov0202@gmail.com>
2025-08-08 11:21:28 +0500

    added new example

--- example_challenge/1-day/app.js
+++ example_challenge/1-day/app.js
@@ -76,3 +76,51 @@
 
 // 7-masala — Juft sonlar yig‘indisi
 // Shart: 1 dan 20 gacha bo‘lgan juft sonlar yig‘indisini toping.
+
+// let total=0
+
+// for(let i=0; i<=20; i+=2){
+//     if(i%2==0){
+//         total+=i
+//     }
+// }
+
+// console.log(total)
+
+// 8-masala — Palindrom so‘z tekshirish
+// Shart: So‘z teskari o‘qilganda ham bir xil bo‘lsa ("level", "anna") palindrom deyiladi. So‘zni tekshiring.
+
+// let str="bahrom"
+// let reversed=str.split('').reverse().join('')
+ 
+//   if(str==reversed){
+//     console.log("Kiritilgan so'z palindrom")
+//   }else{
+//      console.log("Kiritilgan so'z palindrom emas")
+//   }
+
+// 9-masala — Takrorlanuvchi elementlarni topish
+// Shart: [1, 2, 3, 2, 4, 1, 5] massivida takrorlanuvchi sonlarni toping.
+
+// let nums = [1, 2, 3, 2, 4, 1, 5];
+// let duplicates = [];
+//   for(let i=0; i<nums.length; i++){
+//       for(let j=i+1; j<nums.length; j++){
+//          if(nums[i]===nums[j] && !duplicates.includes(nums[i])){
+//             duplicates.push(nums[i])
+//          }
+//       }
+//   }
+
+//   console.log("Takrorlanganlar", duplicates)
+
+// 10-masala — Fibonacci sonlari
+// Shart: 10 ta Fibonacci sonini chiqaring (0, 1, 1, 2, 3, 5, 8...).
+
+
+let n1=0
+let n2=1
+
+for(let i=3; i<10; i++){
+    let 
+}
