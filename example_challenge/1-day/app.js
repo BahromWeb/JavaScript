c21e99c28a04610b1944155920399bba4ea1eb35
BahromWeb <bahromhasanov0202@gmail.com>
2025-08-08 10:54:32 +0500

    new example

---
+++ example_challenge/1-day/app.js
@@ -0,0 +1,78 @@
+// 1-masala — Son juft yoki toq ekanini aniqlash
+// Shart: Foydalanuvchi kiritgan son juft yoki toq ekanini ekranga chiqaring.
+
+// let a=7
+
+// if(a%2==0){
+//     console.log(`${a} son juft`)
+// }else{
+//      console.log(`${a} son toq`)
+// }
+
+
+// 2-masala — Musbat yoki manfiy son
+// Shart: Kiritilgan son musbat, manfiy yoki nol ekanini aniqlang.
+
+// let a=-5
+
+// if(a>0){
+//     console.log(`${a} son musbat`)
+// }else if(a<0){
+//     console.log(`${a} son manfiy`)
+// }else{
+//     console.log(`${a} son 0 ga teng`)
+// }
+
+// 3-masala — 1 dan 10 gacha sonlar yig‘indisi
+// Shart: 1 dan 10 gacha bo‘lgan sonlar yig‘indisini toping.
+
+
+// let sum=0
+
+// for(let i=1; i<=10; i++){
+//     sum+=i
+// }
+
+// console.log(sum)
+
+// 4-masala — Eng katta son
+// Shart: Uchta son berilgan. Eng kattasini aniqlang.
+
+// let a=4, b=7, c=0
+
+// let max=0
+
+// if(a>=b && a>=c){
+//     max=a
+// }else if(b>=a && b>=c){
+//     max=b
+// }else{
+//     max=c
+// }
+
+
+// console.log(max)
+
+// 5-masala — Massivdagi eng kichik son
+// Shart: [3, 5, 1, 9, -4] massivdagi eng kichik sonni toping.
+
+// let arr = [3, 5, 1, 9, -4];
+// let min = arr[0];
+
+// for(let i=1; i<arr.length; i++){
+//      if(arr[i]<min){
+//         min=arr[i]
+//      }
+// }
+
+// console.log(min)
+
+// 6-masala — Matnda belgi sonini hisoblash
+// Shart: "salom" so‘zida nechta belgi borligini toping.
+
+// let a="salom"
+
+// console.log(a.length)
+
+// 7-masala — Juft sonlar yig‘indisi
+// Shart: 1 dan 20 gacha bo‘lgan juft sonlar yig‘indisini toping.
