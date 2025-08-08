35e5d04bdb9e2c20c87fa2821db04cf03a595759
BahromWeb <bahromhasanov0202@gmail.com>
2025-08-08 22:41:50 +0500

    added new example

---
+++ example_challenge/2-day/app.js
@@ -0,0 +1,23 @@
+// 1-masala — Sonlar kvadratini chiqarish
+// Shart: 1 dan 5 gacha bo‘lgan sonlarning kvadratini ekranga chiqaring.
+
+// for(let i=0; i<5; i++){
+//     console.log(i,i*i)
+// }
+
+// let harf="B"
+
+// if(harf==harf.toUpperCase()){
+//     console.log(`${harf} Katta harf`)
+// }else{
+//     console.log(`${harf} kichkina harf`)
+// }
+
+// 3-masala — 5 ga karrali sonlar
+// Shart: 1 dan 50 gacha bo‘lgan faqat 5 ga karrali sonlarni chiqaring.
+
+// for(let i=1; i<50; i++){
+//     if(i%5==0){
+//         console.log(i)
+//     }
+// }
