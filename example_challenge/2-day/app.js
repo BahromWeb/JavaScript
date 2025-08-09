cc50f2098de7a098b885f91b97977617b5d74be2
BahromWeb <bahromhasanov0202@gmail.com>
2025-08-09 09:47:03 +0500

    added new example

--- example_challenge/2-day/app.js
+++ example_challenge/2-day/app.js
@@ -21,3 +21,41 @@
 //         console.log(i)
 //     }
 // }
+
+
+// 4-masala — Massivdagi barcha sonlar yig‘indisi
+// Shart: [4, 7, 1, 3] massivining yig‘indisini hisoblang.
+
+// let arr=[23, 4, 56, 7, 8, 0, 22]
+// let sum=0;
+
+// for(let i=0; i<arr.length; i++){
+//     sum+=arr[i]
+// }
+
+// console.log(sum)
+
+// 5-masala — 3 ga va 5 ga bo‘linadigan sonlar
+// Shart: 1 dan 30 gacha bo‘lgan faqat 3 va 5 ga bo‘linadigan sonlarni chiqaring.
+
+// for(let i=1; i<30; i++){
+//     if(i%3==0 && i%5==0){
+//         console.log(i)
+//     }
+// }
+
+// 6-masala — Matndagi so‘zlar soni
+// Shart: "Men JavaScript o‘rganayapman" jumlasida nechta so‘z borligini toping.
+
+// let sentence = "Men JavaScript organayapman";
+
+// let word=sentence.split(" ")
+
+// console.log(word.length)
+
+// 7-masala — Teskari massiv
+// Shart: [1, 2, 3, 4] massivini teskari tartibda chiqaring.
+
+let num=[1, 2, 3, 4]
+
+console.log(num.reverse())
