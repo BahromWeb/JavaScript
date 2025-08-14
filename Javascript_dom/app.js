cde2cd0edf45366f86bbfa408dd47c7a7fe1c5aa
BahromWeb <bahromhasanov0202@gmail.com>
2025-08-14 09:54:56 +0500

    added new example

--- Javascript_dom/app.js
+++ Javascript_dom/app.js
@@ -1,18 +1,43 @@
 
 
 
-let company={
-    sales:[
-        {name:"Bahrom", salary:2000},
-        {name:"Jamshid", salary:3000}
-    ], 
-    development:{
-        sites:[
-            {name:"Umid", salary:3000},
-            {name:"Alimurod", salary:5000}
-        ],
-        internals:[{name:"Jack", salary:5000}]
-    }
+let project = {
+  frontend: [
+    { name: "John", hours: 35 },
+    { name: "Alice", hours: 42 }
+  ],
+  backend: {
+    api: [
+      { name: "Sam", hours: 50 },
+      { name: "Olga", hours: 45 },
+      { subApi: [{ name: "Kate", hours: 25 }] }
+    ],
+    db: [
+      { name: "Max", hours: 30 },
+      { name: "Nick", hours: 20 }
+    ]
+  }
+};
+
+
+const sumHours=(data)=>{
+     let total=0;
+     if(Array.isArray(data)){
+        for(let item of data){
+            if(typeof item==="object" && item!=null){
+                if("hours" in item){
+                    total+=item.hours
+                }else{
+                    total+=sumHours(item)
+                }
+            }
+        }
+     }else if(typeof data==="object" && data!=null){
+        for(let value of Object.values(data)){
+            total+=sumHours(value)
+        }
+     }
+     return total
 }
 
-
+console.log(sumHours(project)); // 247
